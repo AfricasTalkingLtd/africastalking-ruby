8f6e7f631e4dc417f951d03261423afc75eb3f89
Michael Mwirigi <mwirigimichael@gmail.com>
2018-03-15 13:18:26 +0300

    other product method accessible via gateway class instance

--- lib/Airtime.rb
+++ lib/Airtime.rb
@@ -8,7 +8,12 @@ require 'pry'
 module AfricasTalking
 	
 	# /////////////////////////
-	class Airtime < Gateway
+	class Airtime
+		HTTP_CREATED     = 201
+		HTTP_OK          = 200
+
+		#Set debug flag to to true to view response body
+		DEBUG            = true
 		def initialize username, apikey, environment = nil
 			@username    = username
 			@apikey      = apikey
@@ -43,6 +48,40 @@ module AfricasTalking
 		private
 			def getAirtimeUrl()
 				return getApiHost() + "/version1/airtime"
+			end
+
+			def getApiHost()
+				if(@environment == "sandbox")
+					return "https://api.sandbox.africastalking.com"
+				else
+					return "https://api.africastalking.com"
+				end
+			end
+
+			def executePost(url_, data_ = nil)
+				uri		 	     = URI.parse(url_)
+				http		     = Net::HTTP.new(uri.host, uri.port)
+				http.use_ssl     = true
+				headers = {
+				   "apikey" => @apikey,
+				   "Accept" => "application/json"
+				}
+				if(data_ != nil)
+					request = Net::HTTP::Post.new(uri.request_uri)
+					request.set_form_data(data_)
+				else
+				    request = Net::HTTP::Get.new(uri.request_uri)
+				end
+				request["apikey"] = @apikey
+				request["Accept"] = "application/json"
+				response          = http.request(request)
+
+				if (DEBUG)
+					puts "Full response #{response.body}"
+				end
+
+				@response_code = response.code.to_i
+				return response.body
 			end
 	end
 	class AirtimeResult
--- lib/Payments.rb
+++ lib/Payments.rb
@@ -8,7 +8,14 @@ require 'pry'
 module AfricasTalking
 	
 	# /////////////////////////
-	class Payments < Gateway
+	class Payments
+
+		HTTP_CREATED     = 201
+		HTTP_OK          = 200
+
+		#Set debug flag to to true to view response body
+		DEBUG            = true
+
 		BANK_CODES = {
 	      'FCMB_NG' => 234001,
 	      'ZENITH_NG' => 234002,
@@ -93,7 +100,7 @@ module AfricasTalking
 			}
 			
 			url      = getMobilePaymentB2BUrl()
-			response = self.sendJSONRequest(url, parameters)
+			response = sendJSONRequest(url, parameters)
 			
 			if (@response_code == HTTP_CREATED)
 				resultObj = JSON.parse(response, :quirky_mode =>true)
@@ -338,7 +345,35 @@ module AfricasTalking
 			def getTopupStashUrl()
 				return getPaymentHost() + "/topup/stash"
 			end
+
+			def getApiHost()
+				if(@environment == "sandbox")
+					return "https://api.sandbox.africastalking.com"
+				else
+					return "https://api.africastalking.com"
+				end
+			end
 			
+			def sendJSONRequest(url_, data_)
+				uri	       = URI.parse(url_)
+				http         = Net::HTTP.new(uri.host, uri.port)
+				http.use_ssl = true
+				req          = Net::HTTP::Post.new(uri.request_uri, 'Content-Type'=>"application/json")
+				
+				req["apikey"] = @apikey
+				req["Accept"] = "application/json"
+				
+				req.body = data_.to_json
+
+				response  = http.request(req)
+				
+				if (DEBUG)
+					puts "Full response #{response.body}"
+				end
+
+				@response_code = response.code.to_i
+				return response.body
+			end
 	end
 	
 	class MobileB2CResponse
